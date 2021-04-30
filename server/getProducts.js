cb087197f599037f17638f087274cde6dae65bae
Ashley Vick <ashleyvick@github.com>
2021-04-29 19:16:07 -0600

    forgot to .send products on line 10

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,33 +1,13 @@
-const products = require('../products.json')
-
-
-// const getProducts = (req, res) => {
-//     const item = products.find((elem) => elem.id === parseInt(req.params.id))
-//     if (!item) {
-//         return res.status(500).send('Item is not in the list')
-//     } else {
-//     res.status(200).send(item)
-//     }
-// }
+const products = require("../products.json");
 
 const getProducts = (req, res) => {
-    const { price } = req.query
+  const { price } = req.query;
 
-    if (price) {
-        const items = products.filter(e => e.price <= parseInt(price))
-        return res.status(200).send(items)
-    } 
-}
+  if (price) {
+    const items = products.filter((e) => e.price >= parseInt(price));
+    return res.status(200).send(items);
+  }
+  res.status(200).send(products);
+};
 
-
-
-// const getProducts = (req, res) => {
-//     if (req.query.price){
-//         const items = products.filter((elem) => elem.price <= parseInt(req.query.price))
-//         return res.status(200).send(items)
-//     }
-// }
-
-
-
-module.exports = getProducts
+module.exports = getProducts;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const products = require('../products.json')
-const getProducts = require('../server/getProducts')
+const express = require("express");
+const products = require("../products.json");
+const getProducts = require("../server/getProducts");
 
-const app = express()
+const app = express();
 
-const port = 8476
+const port = 5050;
 
-app.get('/api/products', getProducts)
+app.get("/api/products", getProducts);
 
-app.get('/api/products/:id', getProducts)
-console.log(products)
+app.get("/api/products/:id", getProducts);
+console.log(products);
 
-app.listen(port, ()=>{
-    console.log(`Server is still listening on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is still listening on port: ${port}`);
+});
